98a34d3199ec64ad9a09257d1085de27ffa36cdb
dewikiswani <dewikiswanibodro@gmail.com>
2020-01-21 17:26:45 +0700

    revision section and assumption capital data

--- syntax/rcode penghitungan tanpa shiny.R
+++ syntax/rcode penghitungan tanpa shiny.R
@@ -3,6 +3,7 @@ library(FinCal)
 library(rio)
 library(dplyr)
 library(stringr)
+library(tidyverse)
 #AE COMMENT1: saran saya mungkin package 'rio' tidak diperlukan sekiranya hanya
 #akan digunakan untuk menyimpan dan memanggilRData saja. Saran lain adalah
 #mungkin Dewi bisa explore package 'rtf' untuk membuat report secara otomatis
@@ -22,107 +23,138 @@ library(stringr)
 
 # Section1: Input Data ----------------------------------------------------
 
-setwd("D:/1_Profitability/data")
-data1<-read.csv("data1.csv")
-data2<-read.csv("data2.csv")
-data3<-read.csv("data3.csv")
+#set directory
+setwd("C:/dw/ICRAF/profitability/data/data clean")
+
+#data
+io.in <-read.csv("io.in.csv",stringsAsFactors = F)
+io.out <- read.csv("io.out.csv",stringsAsFactors = F)
+price.in <- read.csv("price.in.csv",stringsAsFactors = F)
+price.out <- read.csv("price.out.csv",stringsAsFactors = F)
+
+
+#data capital 
+## ada 2 kondisi
+### kondisi 1 [cap.contition=="no"]: capital setiap tahun berbeda tiap unitnya, dengan nilai yg berbeda
+### kondisi 2 [cap.condition=="repeat"]: capital setiap tahun berulang tiap tahunnya(konsisten), dengan nilai sama yang berulang
+#capital <- NULL
+cap.condition <- scan(what = "character") #isikan repeat atau no lalu enter 2 kali
+
+######## ISI DULU KONDISI DI ATAS 
+if(cap.condition=="no"){
+  capital<- read.csv("capital.p.csv",stringsAsFactors = F) 
+}else if (cap.condition=="repeat"){
+  capital <- read.csv("capital.repeat.csv", stringsAsFactors = F)
+}else{
+  print("ERROR: Kondisi Modal Kapital harus ditentukan") #case utk capital yg tidak ada isinya belum dibuat
+}
+
+
+#variabel input
+n=30 #jumlah tahun
+rate.p <- 7 #diskon rate private
+rate.s <- rate.p - 5 #diskon rate social
+profit0 <- 0 
+nilai.tukar <- 10000 #nilai tukar rupiah thd USD
 
 
 # Section2: Data Pre-Processing -------------------------------------------
 
 #manipulasi data1
-data1 <- cbind(Status="General", data1)
+data1 <- rbind(io.in,io.out) #combine all data input-output
+data1 <- cbind(Status="General", data1) #add variable status
+data1 <- data1 %>% mutate_if(is.factor,as.character) #change factor var to char var
+  
 #AE COMMENT6: code pada line 30 diatas akan membuat kolom dengan class "factor". Apakah benar seperti itu yang diinginkan?
 
-#manipulasi data2
+# manipulasi data2
 ## asumsi data capital untuk private = social
-
-## AE COMMENT 7: Asumsi pada line 34 diatas, perlu dimasukkan sebagai input di
+## AE COMMENT 7: Asumsi pada line diatas, perlu dimasukkan sebagai input di
 ## Section1, dengan demikian, user akan diberi pilihan apakah mereka ingin
 ## memasukkan Private Capital yang sama persis dengan Social Capital.
 ## Berdasarkan input tersebut, line coding dibawah akan dijalankan.
 
-pcap <- cbind(Status="Private Budget", data2)
-scap <- cbind(Status="Social Budget", data2)
+cond.sosial <- scan(what = "character") #isikan beda atau sama lalu enter 2 kali
+
+if(cap.condition == "no" & cond.sosial == "sama"){
+  pcap = capital
+  pcap <- cbind(Status="Private Budget", pcap)
+  pcap <- pcap %>% mutate_if(is.factor,as.character)
+  
+  scap = capital 
+  scap <- cbind(Status="Social Budget", scap)
+  scap <- scap %>% mutate_if(is.factor,as.character) 
+}else if (cap.condition == "no"  & cond.sosial == "beda"){
+  scap = capital.s #jika capital social != dan hrus upload data baru
+  scap <- cbind(Status="Social Budget", scap)
+  scap <- scap %>% mutate_if(is.factor,as.character)
+} else if (cap.condition == "repeat"){
+  pcap<-capital[-6]
+  pcap.year<-data.frame(replicate(n,capital[[5]])) #replicate nilai private price sebanyak n tahun
+  colnames(pcap.year)<-paste0(c(rep("Y", n)),1:n)
+  pcap<-cbind(Status="Private Budget" ,pcap[c(1:4)],pcap.year)
+  pcap <- pcap %>% mutate_if(is.factor,as.character)
+  scap<-capital[-5]
+  scap.year<-data.frame(replicate(n,scap[[5]]))
+  colnames(scap.year)<-paste0(c(rep("Y", n)),1:n)
+  scap<-cbind(Status="Social Price",scap[c(1:4)],scap.year)
+  scap <- scap %>% mutate_if(is.factor,as.character) #change factor var to char 
+}
+
 
 # manipulasi data3 = data price
+data3 <- rbind(price.in,price.out)
 p.price<-data3[-6]
-n=30
+#n=30
 # AE COMMENT 8: jumlah tahun sebaiknya dimasukkan sebagai input di dalam Section1
-p.year<-data.frame(replicate(n,p.price$Private.Price))
+p.year<-data.frame(replicate(n,p.price$Private.Price)) #replicate nilai private price sebanyak n tahun
 colnames(p.year)<-paste0(c(rep("Y", n)),1:n)
-p.price<-cbind(Status="Private Price",p.price[c(1:4)],p.year)
+p.price<-cbind(Status="Private Price" ,p.price[c(1:4)],p.year)
+p.price <- p.price %>% mutate_if(is.factor,as.character) #change factor var to char var
 
 s.price<-data3[-5]
-n=30
 s.year<-data.frame(replicate(n,s.price$Social.Price))
 colnames(s.year)<-paste0(c(rep("Y", n)),1:n)
 s.price<-cbind(Status="Social Price",s.price[c(1:4)],s.year)
+s.price <- s.price %>% mutate_if(is.factor,as.character) #change factor var to char 
 
 # AE COMMENT 9: Really nice work on those lines above !!! Well done.
 
 
-# Section 3: Save to database ---------------------------------------------
-
 ## all data
 #untuk sementara
-#data.gab<-rbind(data1,p.price,s.price,p)
 data.gab<-rbind(data1,p.price,s.price,pcap,scap)
 
-# AE COMMENT 10: Pada line diatas, sebaiknya juga ditambahkan kolom-kolom yang
-# memuat informasi tentang komoditas yang sedang dianalisa, saran saya,
-# tambahkan kolom: nama komoditas, sistem usaha tani, lokasi-provinsi,
-# lokasi-kabupaten, lokasi-desa dan nama surveyor
 
-#export to rdata
-export(data.frame(data.gab), "1.rdata")
-
-# AE COMMENT 11: Selain Rdata, Dewi juga punya pilihan untuk menggunakan fungsi
-# saveRDS disini. Sebaiknya Dewi juga sudah mulai mengusulkan sistem penamaan
-# yang sistematis, mungkin informasi yang saya usulkan di comment 10 bisa dijadikan
-# bahan pemikiran
-
-# Section 4: Load database ------------------------------------------------
-
-#import rdata 
-load("1.rdata", gab <- new.env())
-ls.str(gab)
-
-# AE COMMENT 12: pada bagian ini, dewi juga mungkin sudah bisa memikirkan
-# bagaimana cara pemanggilan yang baik pada saat Rdata yang tersimpan sudah
-# banyak, mungkin perlu ditambahkan input seperti yang ada pada comment 10 untuk
-# menentukan file mana yang akan dipanggil.
-
-
-# Section 5: Data processing ----------------------------------------------
+# Section 3: Data processing ----------------------------------------------
 
 # AE COMMENT 13: Dewi, walaupun saya letakkan section divider diatas, sebenarnya
-# section 5 ini lebih pas kalau diletakkan sebelum section 4. Sehingga Rdata
+# section (data processing) ini lebih pas kalau diletakkan sebelum section (save to database). Sehingga Rdata
 # yang dihasilkan, selian menyimpan data mentah, juga menyimpan hasil
 # perhitungan NPV dll
 
 #perkalian antara general dan Private Price
-a <- filter(gab$x, Status == c("General"))
-b <- filter(gab$x, Status == c("Private Price"))
-p.budget <- a[-(1:5)] * b[-c(1:5)]
-p.budget <- cbind(a[1:5],p.budget)
+a <- filter(data.gab,Status == c("General")) #filter data input output (yg sudah diberi status=general)
+b <- filter(data.gab, Status == c("Private Price")) #filter data private price
+p.budget <- a[-(1:5)] * b[-c(1:5)] #perkalian antara unit pada tabel io dg price tanpa variabel 1 sd 5
+p.budget <- cbind(a[1:5],p.budget) #memunculkan kembali variabel 1 sd 5 
 p.budget <- p.budget %>% 
-  mutate(Status = case_when(Status == "General" ~ "Private Budget", TRUE ~ as.character(Status)))
+  mutate(Status = case_when(Status == "General" ~ "Private Budget")) #mengubah status yg General mjd Private Budget
 
 #perkalian antara general dengan Social Price
-c <- filter(gab$x, Status == c("Social Price"))
+c <- filter(data.gab, Status == c("Social Price")) #filter data social price
 s.budget <- a[-(1:5)] * c[-c(1:5)]
 s.budget <- cbind(a[1:5],s.budget)
 s.budget <- s.budget %>% 
-  mutate(Status = case_when(Status == "General" ~ "Social Budget", TRUE ~ as.character(Status)))
+  mutate(Status = case_when(Status == "General" ~ "Social Budget"))
 
 #AE COMMENT 14: Nice works for the lines above!!
 
-#penggabungan dengan capital
-p.cap <- filter(gab$x, Status == c("Private Budget"))
+#penggabungan dengan data capital
+p.cap <- filter(data.gab, Status == c("Private Budget"))
 p.budget <- rbind(p.budget,p.cap)
 
-s.cap <- filter(gab$x, Status == c("Social Budget"))
+s.cap <- filter(data.gab, Status == c("Social Budget"))
 s.budget <- rbind(s.budget,s.cap)
 
 ################ penghitungan NPV
@@ -149,12 +181,12 @@ s.sum.rev <- s.rev.output[,-(1:5)] %>%
 
 p.profit <- p.sum.rev - p.sum.cost        
 s.profit <- s.sum.rev - s.sum.cost 
-profit0 <- 0
+#profit0 <- 0
 p.profit<-c(profit0,p.profit)
 s.profit<-c(profit0,s.profit)
 
-npv.p<-npv(7/100,p.profit)
-npv.s<-npv(2/100,s.profit)
+npv.p<-npv(rate.p/100,p.profit)
+npv.s<-npv(rate.s/100,s.profit)
 
 # AE COMMENT 15: suku bunga 7% dan 2% diatas, perlu dimasukkan sebagai data
 # input pada section1, mohon diingat bahwa suku bunga bisa bervariasi dalam
@@ -162,7 +194,7 @@ npv.s<-npv(2/100,s.profit)
 
 hsl.npv<-data.frame(PRIVATE=npv.p,SOCIAL=npv.s)
 
-nilai.tukar <- 10000
+#nilai.tukar <- 10000
 # AE COMMENT 16: nilai tukar usd=10000 idr, perlu dimasukkan sebagai data input
 # pada section1, mohon diingat bahwa nilai tukar bisa bervariasi dalam 30 tahun.
 # Usul saya, perhitungan ini sebaiknya diintegrasikan kedalam perhitungan cost
@@ -204,13 +236,13 @@ ec <- p.sum.cost[[1]]
 
 
 ############# PERHITUNGAN HARVESTING PRODUCT
-e <- filter(gab$x, Status == c("General"))
-fil.prod <- e %>%  filter(str_detect(Grup,"Output"))
+e <- a
+fil.prod <- e %>%  filter(str_detect(Grup,"Output")) #filter io untuk grup output (hasil panen)
 sum.prod <- fil.prod[,-(1:5)] %>% 
   colSums(na.rm = T)
 tot.prod <- sum(sum.prod)
 
-fil.labor <- e %>%  filter(str_detect(Var1,"Tenaga Kerja"))
+fil.labor <- e %>%  filter(str_detect(Var1,"Tenaga Kerja")) #filter io untuk var labor
 sum.labor <- fil.labor[,-(1:5)] %>% 
   colSums(na.rm = T)
 tot.labor <- sum(sum.labor)
@@ -221,11 +253,38 @@ hp <- tot.prod/tot.labor
 
 
 ############# PERHITUNGAN LABOR REQ FOR EST
-lr <- sum.labor[[1]]
+lr <- sum.labor[[1]] #pekerja pada tahun 1
 
 gab <- list(hsl.npv,nlc,hp,lr)
 gab
 
+
+# Section 4: Save to database ---------------------------------------------
+
+
+# AE COMMENT 10: Pada line diatas, sebaiknya juga ditambahkan kolom-kolom yang
+# memuat informasi tentang komoditas yang sedang dianalisa, saran saya,
+# tambahkan kolom: nama komoditas, sistem usaha tani, lokasi-provinsi,
+# lokasi-kabupaten, lokasi-desa dan nama surveyor
+
+#export to rdata
+#export(data.frame(data.gab), "1.rdata")
+
+# AE COMMENT 11: Selain Rdata, Dewi juga punya pilihan untuk menggunakan fungsi
+# saveRDS disini. Sebaiknya Dewi juga sudah mulai mengusulkan sistem penamaan
+# yang sistematis, mungkin informasi yang saya usulkan di comment 10 bisa dijadikan
+# bahan pemikiran
+
+# Section 5: Load database ------------------------------------------------
+
+#import rdata 
+#load("1.rdata", gab <- new.env())
+#ls.str(gab)
+
+# AE COMMENT 12: pada bagian ini, dewi juga mungkin sudah bisa memikirkan
+# bagaimana cara pemanggilan yang baik pada saat Rdata yang tersimpan sudah
+# banyak, mungkin perlu ditambahkan input seperti yang ada pada comment 10 untuk
+# menentukan file mana yang akan dipanggil.
 
 # Section 6 : report ------------------------------------------------------
 
